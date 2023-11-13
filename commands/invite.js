0b47bf94720a0df87850e9b1f0e53a4d75fff85b
CodeCraft <147920942+CodeCraft56@users.noreply.github.com>
2023-11-13 07:33:30 -0500

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,7 @@
+module.exports.run = async(client, message, args) => {
+message.reply(`Invite me to your server: https://discord.com/api/oauth2/authorize?client_id=1168178543138705550&permissions=67584&scope=bot`) //Your invite link
+}
+module.exports.config = {
+  name: 'invite',
+  aliases: ['in']
+}
